a153cc67688ecf52732c89f66fc731f70a8e88da
Abubakar Siddique <abz.siddique@gmail.com>
2021-06-18 10:48:01 +0100

    feat: Create a helper function for headers

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,22 @@
 import axios from "axios"
+import { getToken } from "./auth"
+
+
+function headers() {
+  return {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  }
+}
+
+
+// * Learn Requests
+
+export function createCourse(formdata) {
+  return axios.post('api/courses/', formdata, headers())
+}
+
+
+// * Auth Requests
 
 export function registerUser(formData) {
   return axios.post('api/auth/register/', formData)
