65f3d4041e97ffda8de807ca78bc70f51cec8700
“rizwanakhtar7” <rizwanakhtar@live.co.uk>
2021-06-21 16:18:07 +0100

    adding buttons for subjects

--- src/components/common/Course/CourseIndex.js
+++ src/components/common/Course/CourseIndex.js
@@ -5,33 +5,49 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlusCircle, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { isAuthenticated } from '../../../lib/auth'
 import { Link } from 'react-router-dom'
+import { createCourse } from '../../../lib/api'
 
 function CourseIndex() {
   const [courses, setCourses] = React.useState([])
   const [averageStars, setAverageStars] = React.useState([])
   const isLoggedIn = isAuthenticated()
-  // const [selectedSubjects, setSelectedSubjects] = React.useState([])
-
-
-  //function to filter down by subject categories when user clicks on button
-  // const filteredSubjects = courses?.filter(course => {
-  //   if (!selectedSubjects.length) {
-  //     return true
-  //   }
+  const [selectedSubjects, setSelectedSubjects] = React.useState([])
+  const subjects = ['Computing', 'Math', 'Science', 'English']
   
-  //   return selectedSubjects.every(selectedSubject => course.subject.map(s => s).includes(selectedSubject) )
-    
-  // })
-  const handleSearch = async (e) => {
-    try {
-      const { data } = await axios.get(`/api/courses/?search=${e.target.value}`) 
-      setCourses(data)
-      // console.log(data)
 
-    } catch(e) {
-      console.log(e)
+  // Search bar -- Remove if not required
+  // const [search, setSearch] = React.useState('')
+
+  // const handleSearch = (e) => {
+  //   setSearch(e.target.value)
+  // }
+
+  // const filteredResult = courses && courses.filter(course => {
+  //   return (
+  //     course.name.toLowerCase().includes(search.toLowerCase()) || 
+  //     course.description.toLowerCase().includes(search.toLowerCase())
+  //   )
+  // })
+
+
+  const handleClick = e => {
+    if (selectedSubjects.includes(e.target.value)) {
+      return setSelectedSubjects(selectedSubjects.filter(subject => subject !== e.target.value))
     }
+    setSelectedSubjects([...selectedSubjects, e.target.value]) 
+
   }
+
+
+  // function to filter down by subject categories when user clicks on button
+  const filteredResult = courses?.filter(course => {
+    if (!selectedSubjects.length) {
+      return true
+    }
+    return selectedSubjects.every(selectedSubject => course.subject.includes(selectedSubject))
+    
+  })
+  
 
   //TESTING AVERAGE STAR RATING 
   // React.useEffect(() => {
@@ -47,7 +63,6 @@ function CourseIndex() {
   //   getUserRatings()
   // },[])
   // console.log(courses.subject.map(subject => subject))
-  courses.map(course => console.log(course.subject))
 
   React.useEffect(() => {
     const getData = async (e) => {
@@ -62,6 +77,8 @@ function CourseIndex() {
     getData()
   },[])
 
+  console.log(courses.name)
+
   return (
     <>
       <div className="margin-top-spacer">
@@ -71,13 +88,24 @@ function CourseIndex() {
         <FontAwesomeIcon icon={faSearch} />
         <input onChange={handleSearch} placeholder="Search.." />
       </div>
-      <div>
-       
+      <div >
+        {subjects.map((subject) => (
+          <button
+            className="subjects-btn" 
+            key={subject}
+            value={subject}
+            onClick={handleClick}
+            className={`subject-btn ${selectedSubjects.includes(subject)? 'btn-active' : ''}`}
+          >
+            {subject}
+
+          </button>
+        ))}
       </div>
 
       <div className="main-container">
         <div className="course-container">
-          {courses.map(course => <CourseCard key={course._id} {...course} /> )}
+          {filteredResult?.map(course => <CourseCard key={course._id} {...course} /> )}
         </div>
         { isLoggedIn && (
         <>
