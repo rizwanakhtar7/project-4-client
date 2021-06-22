f7083eb0648296fe9ba39a66d5693ff37ab7413d
Abubakar Siddique <abz.siddique@gmail.com>
2021-06-22 05:52:17 +0100

    feat: Add form error handling to post a new course

--- src/components/common/Course/CourseNew.js
+++ src/components/common/Course/CourseNew.js
@@ -5,7 +5,7 @@ import ImageUpload from '../ImageUpload'
 
 function CourseNew() {
 
-  const { formData, handleChange } = useForm({
+  const { formData, handleChange, formErrors, setFormErrors } = useForm({
     title: '',
     description: '',
     name: '',
@@ -24,6 +24,7 @@ function CourseNew() {
       await createCourse(formData)
     } catch (err) {
       console.log(err)
+      setFormErrors(err.response.data)
     }
   }
 
@@ -33,22 +34,27 @@ function CourseNew() {
       <div className="container">
         <form className="container" onSubmit={handleSubmit}>
           <label htmlFor="title">Course Title:</label><br />
-          <input type="text" id="title" name="title" placeholder="Course Title" value={formData.title} onChange={handleChange} /><br />
+          <input type="text" id="title" name="title" placeholder="Course Title" value={formData.title} onChange={handleChange} />
+          {formErrors.title && <small>Course Title is Required</small>}<br />
 
           <label htmlFor="description">Course Description:</label><br />
-          <input type="text" id="description" name="description" placeholder="Course Description" value={formData.description} onChange={handleChange} /><br />
-          
+          <input type="text" id="description" name="description" placeholder="Course Description" value={formData.description} onChange={handleChange} />
+          {formErrors.description && <small>Course Description is Required</small>}<br />
+
           <label htmlFor="courseImage">Course Image:</label><br />
           <div>
             <ImageUpload onUpload={handleImageUpload} />
           </div>
+          {formErrors.courseImage && <small>Course Image is Required</small>}<br />
           <br />
 
           <label htmlFor="name">Course Name:</label><br />
-          <input type="text" id="name" name="name" placeholder="Course Name" value={formData.courseName} onChange={handleChange} /><br />
+          <input type="text" id="name" name="name" placeholder="Course Name" value={formData.courseName} onChange={handleChange} />
+          {formErrors.name && <small>Course Name is Required</small>}<br />
 
           <label htmlFor="subject">Course Subject:</label><br />
-          <input type="text" id="subject" name="subject" placeholder="Course Subject" value={formData.courseSubject} onChange={handleChange} /><br /><br />
+          <input type="text" id="subject" name="subject" placeholder="Course Subject" value={formData.courseSubject} onChange={handleChange} />
+          {formErrors.subject && <small>Course Subject is Required</small>}<br />
 
           <input type="submit" value="Submit" />
         </form>
